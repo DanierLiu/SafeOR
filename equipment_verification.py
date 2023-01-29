777359ba26ed5ec3ababbc47646cfeaba33a55aa
DenialHandrails <danielhonrales@gmail.com>
2023-01-29 11:01:31 -0600

    Cleaning up files

--- equipment_verification.py
+++ equipment_verification.py
@@ -16,7 +16,7 @@ doc_ref = db.collection(u'equipment').document(u'fork')
 doc_ref.update({u'requested':False})
 doc_ref = db.collection(u'equipment').document(u'scalpel')
 doc_ref.update({u'requested':False})
-asyncio.run(monitor_fatigue())
+#asyncio.run(monitor_fatigue())
 while True:
     start_audio_recog(db)
     time.sleep(2)
@@ -24,12 +24,12 @@ while True:
     doc_ref = db.collection(u'equipment').document(u'fork')
     fork_data = doc_ref.get().to_dict()
     if fork_data['requested'] != fork_data['taken']: 
-        print('fork alert')
+        print('-----\nWarning: Miscommuncation with fork!\n-----')
     
     doc_ref = db.collection(u'equipment').document(u'scalpel')
     scalpel_data = doc_ref.get().to_dict()
     if scalpel_data['requested'] != scalpel_data['taken']:
-        print('scalpel alert')
+        print('-----\nWarning: Miscommunication with scalpel!\n-----')
 
     doc_ref = db.collection(u'equipment').document(u'fork')
     doc_ref.update({u'requested':False})
